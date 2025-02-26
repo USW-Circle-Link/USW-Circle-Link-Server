1a02083038a04e17332e2ff37f3c5688781b2a1a
Hyeok <bh1848@naver.com>
2025-02-27 06:22:46 +0900

    fix: 동아리명 dto 수정

--- src/main/java/com/USWCicrcleLink/server/admin/admin/dto/AdminClubCreationRequest.java
+++ src/main/java/com/USWCicrcleLink/server/admin/admin/dto/AdminClubCreationRequest.java
@@ -37,7 +37,7 @@ public class AdminClubCreationRequest {
 
     @NotBlank(message = "동아리명은 필수 입력 값입니다.")
     @Size(max = 10, message = "동아리명은 10자 이내여야 합니다.")
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9\\s]+$", message = "동아리명에는 특수문자를 포함할 수 없습니다.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "동아리명에는 공백 또는 특수문자를 포함할 수 없습니다.")
     @Sanitize
     private String clubName;
 

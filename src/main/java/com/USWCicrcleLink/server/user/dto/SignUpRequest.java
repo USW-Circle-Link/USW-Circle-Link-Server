e5b87f2a141783de62c9500d8adf9acdfb268f02
hanjh <wg1004s@naver.com>
2025-03-05 17:40:53 +0900

    fix: 비밀번호 필드에 이모지 입력되지 않도록 수정

--- src/main/java/com/USWCicrcleLink/server/user/dto/SignUpRequest.java
+++ src/main/java/com/USWCicrcleLink/server/user/dto/SignUpRequest.java
@@ -29,7 +29,7 @@ public class SignUpRequest {
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.",groups = ValidationGroups.NotBlankGroup.class)
     @Size(min = 8, max = 20, message = "비밀번호는 8~20자 이내여야 합니다.",groups = ValidationGroups.SizeGroup.class)
     @Pattern(
-            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(?!.*\\s).*$",
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(?!.*\\s)(?!.*[\\p{So}\\p{Cn}]).*$",
             message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 하며 공백을 포함할 수 없습니다.",
             groups = ValidationGroups.PatternGroup.class
     )

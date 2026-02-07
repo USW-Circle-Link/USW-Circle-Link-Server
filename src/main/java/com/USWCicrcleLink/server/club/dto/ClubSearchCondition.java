9ab77092a4016811a439dcdbbb540e04e83d37b8
junse11 <izim5223@gmail.com>
2026-02-08 04:14:12 +0900

    Fix: Add missing ClubSearchCondition class

---
+++ src/main/java/com/USWCicrcleLink/server/club/dto/ClubSearchCondition.java
@@ -0,0 +1,20 @@
+package com.USWCicrcleLink.server.club.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClubSearchCondition {
+    private Boolean open;
+    private String filter;
+    private List<UUID> categoryUUIDs;
+    private Boolean includeNumberOfMembers;
+}

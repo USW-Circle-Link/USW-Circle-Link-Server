8d514eb93734999356b3c8253927b159517fa5d1
junse11 <izim5223@gmail.com>
2026-02-08 16:57:06 +0900

    동아리 정보 부분 수정 / /clubs role 별 구분

---
+++ src/main/java/com/USWCicrcleLink/server/club/leader/dto/RecruitmentStatusResponse.java
@@ -0,0 +1,13 @@
+package com.USWCicrcleLink.server.club.leader.dto;
+
+import com.USWCicrcleLink.server.club.domain.RecruitmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecruitmentStatusResponse {
+    private RecruitmentStatus recruitmentStatus;
+}
